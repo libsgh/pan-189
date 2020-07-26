586df6ea93c1779a60c01b53bc627e5a69f29008
libsgh <woiyyng@gmail.com>
2020-07-26 11:44:46 +0800

    first commit

--- static/main.js
+++ static/main.js
@@ -57,7 +57,7 @@ function initTable(path) {
                 }
                 html+="<tr>"+
                     "    <td class=\"file-name\"><a class=\"icon "+style+"\" href=\""+a+"\">"+item["fileName"]+""+n+"</a></td>"+
-                    "    <td class=\"file-size\">"+item["fileSize"]+"</td>"+
+                    "    <td class=\"file-size\">"+ renderSize(item["fileSize"])+"</td>"+
                     "    <td class=\"file-date-modified\">"+item["lastOpTime"]+"</td>"+
                     "</tr>";
             });
@@ -98,4 +98,16 @@ function viewVideo(obj, src, thumb, title, fileType) {
             "subHtml": "<h4>"+title+"</h4>"
         }]
     })
+}
+function renderSize(value){
+    if(null==value||value==''){
+        return "0 Bytes";
+    }
+    var unitArr = new Array("Bytes","KB","MB","GB","TB","PB","EB","ZB","YB");
+    var index=0;
+    var srcsize = parseFloat(value);
+    index=Math.floor(Math.log(srcsize)/Math.log(1024));
+    var size =srcsize/Math.pow(1024,index);
+    size=size.toFixed(2);//保留的小数位数
+    return size+unitArr[index];
 }
